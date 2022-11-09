a53e247102423b2a792342ce808bc88b64eb79a0
Bill Song <songchao456@gmail.com>
2022-11-09 11:46:46 -0500

    add location dropdown list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './css/App.css';
+import 'semantic-ui-css/semantic.min.css'
 import HomePage from "./pages/HomePage"
 import SeatSelectionPage from "./pages/SeatSelectionPage"
 import MovieSelectionPage from "./pages/MovieSelectionPage"
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,75 @@
 import React, {Component} from 'react';
+import '../css/home.css';
+import { Dropdown, Button, Segment } from 'semantic-ui-react'
+
+const locationOptions = [
+    {
+        key: '1',
+        text: 'North York, Ontario, Canada',
+        value: 'North York',
+        image: {avatar: true, src: "/images/pin.png"},
+    },
+    {
+        key: '2',
+        text: 'Toronto, Ontario, Canada',
+        value: 'Toronto, Ontario, Canada',
+        image: {avatar: true, src: "/images/pin.png"},
+    },
+    {
+        key: '3',
+        text: 'Markham, Ontario, Canada',
+        value: 'Markham, Ontario, Canada',
+        image: {avatar: true, src: "/images/pin.png"},
+    },
+    {
+        key: '4',
+        text: 'Hamilton, Ontario, Canada',
+        value: 'Hamilton, Ontario, Canada',
+        image: {avatar: true, src: "/images/pin.png"},
+    },
+    {
+        key: '5',
+        text: 'Vancouver, British Columbia, Canada',
+        value: 'Vancouver, British Columbia, Canada',
+        image: {avatar: true, src: "/images/pin.png"},
+    },
+]
 
 class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: "",
+        };
+
+        // bind upcoming state changes
+        this.onLocationChange = this.onLocationChange.bind(this);
+    }
+
+    onLocationChange(e, {value}){
+        e.persist();
+        this.location = value;
+        console.log(this.location);
+    };
+
     render() {
         return (
+        <>
             <p>Home Page</p>
+            <Segment color='teal' raised>
+                <Dropdown
+                    placeholder='Select a location'
+                    className='locationDropDown'
+                    search
+                    clearable
+                    openOnFocus
+                    selection
+                    options={locationOptions}
+                    onChange={this.onLocationChange}
+                />
+                <Button className="locationButton" inverted color='blue' content='Next' icon='right arrow' labelPosition='right' />
+            </Segment>
+        </>
         )
     }
 }
