2475c5c59f8fba1a4e0f30245c153cd7fd80fc1a
marlon4dashen <alienlql@hotmail.com>
2022-11-10 01:46:05 -0500

    decorate checkout page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 import "../css/checkout.css";
-import { Form, Container, Button, Grid, Input } from "semantic-ui-react";
+import {
+  Form,
+  Container,
+  Button,
+  Grid,
+  Input,
+  Label,
+  Image,
+  Select,
+  Segment,
+} from "semantic-ui-react";
+
+const stateOptions = [
+  { key: "on", text: "ON", value: "ON" },
+  { key: "ab", text: "AB", value: "Alberta" },
+  { key: "qc", text: "QC", value: "Quebec" },
+  { key: "bc", text: "BC", value: "British Columbia" },
+];
 
 class CheckoutPage extends Component {
   constructor(props) {
@@ -15,81 +32,145 @@ class CheckoutPage extends Component {
 
   render() {
     return (
-      <div className="all">
-        <h1> Checkout </h1>
-        <Grid
-          columns={2}
-          divided
-          style={{
-            "margin-top": "3vh",
-          }}
-        >
-          <Grid.Column>
-            <h1> Summary </h1>
-            <div className="description">
-              <img src=""></img>
-              <Container textAlign="left">
-                <h4>Movie Name</h4>
-                <p> Avengers: Final Game </p>
+      <div className="checkout">
+        <Segment raised padded>
+          <Grid columns={2}>
+            <Grid.Column className="summary">
+              <Container
+                textAlign="left"
+                style={{
+                  padding: "5vh 20vh 5vh 20vh",
+                }}
+              >
+                <h3> Ticket Summary </h3>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Image src="/images/avengers.jpg" size="medium" />
+                  <Container className="description">
+                    <Grid columns={2}>
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Container textAlign="left">
+                            <h4>Movie Name</h4>
+                            <p> Avengers: Final Game </p>
+                          </Container>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Container textAlign="right">
+                            <h4>Date and Time</h4>
+                            <p> 11:40pm, Nov 20th </p>
+                          </Container>
+                        </Grid.Column>
+                      </Grid.Row>
+                      <Grid.Row>
+                        <Grid.Column>
+                          <Container textAlign="left">
+                            <h4>Seat Selected</h4>
+                            <p> A23, B46 </p>
+                          </Container>
+                        </Grid.Column>
+                        <Grid.Column>
+                          <Container textAlign="right">
+                            <h4>Location</h4>
+                            <p> Cineplex Ontario </p>
+                          </Container>
+                        </Grid.Column>
+                      </Grid.Row>
+                    </Grid>
+                  </Container>
+                </div>
               </Container>
-              <Container textAlign="left">
-                <h4>Date and Time</h4>
-                <p> 11:40pm, Nov 20th </p>
+            </Grid.Column>
+            <Grid.Column className="paymentInfo">
+              <Container
+                textAlign="left"
+                style={{
+                  padding: "5vh 20vh 5vh 20vh",
+                }}
+              >
+                <h3> Payment Info </h3>
+                <Form className="form">
+                  <Form.Input
+                    label="Card Number"
+                    placeholder="Card Number"
+                    required={true}
+                    width={8}
+                  />
+                  <Form.Input
+                    label="Card Holder"
+                    placeholder="Card Holder"
+                    required={true}
+                    width={8}
+                  />
+                  <Form.Input
+                    label="Email"
+                    placeholder="joe123@abc.com"
+                    required={true}
+                    width={8}
+                  />
+
+                  <Form.Input
+                    label="CVV"
+                    placeholder="CVV"
+                    required={true}
+                    width={3}
+                  />
+
+                  <Form.Group
+                    style={{
+                      display: "block",
+                    }}
+                  >
+                    <label>Billing Address</label>
+                    <Form.Field
+                      control={Input}
+                      label="Street"
+                      placeholder="Street"
+                      required={true}
+                      width={8}
+                    />
+                    <Container
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "left",
+                        width: "100%",
+                      }}
+                    >
+                      <Form.Input
+                        label="Postal Code"
+                        placeholder="XXX XXX"
+                        required={true}
+                        width={4}
+                      />
+                      <Form.Field
+                        control={Select}
+                        options={stateOptions}
+                        required={true}
+                        label={{
+                          children: "State",
+                          htmlFor: "form-select-control-state",
+                        }}
+                        placeholder="State"
+                        search
+                        searchInput={{ id: "form-select-control-state" }}
+                        width={2}
+                      />
+                    </Container>
+                  </Form.Group>
+                </Form>
+                <Container textAlign="center">
+                  <Button type="submit">Proceed Payment</Button>
+                </Container>
               </Container>
-              <Container textAlign="left">
-                <h4>Seat Selected</h4>
-                <p> A23, B46 </p>
-              </Container>
-              <Container textAlign="left">
-                <h4>Location</h4>
-                <p> Cineplex Ontario </p>
-              </Container>
-            </div>
-          </Grid.Column>
-          <Grid.Column>
-            <Container textAlign="center">
-              <h2> Payment Info </h2>
-              <Form className="form">
-                <Form.Input
-                  className="label"
-                  label="Card Number"
-                  placeholder="Card Number"
-                  required={true}
-                  width={8}
-                />
-                <Form.Input
-                  className="label"
-                  label="Card Holder"
-                  placeholder="Card Holder"
-                  required={true}
-                  width={8}
-                />
-                <Form.Input
-                  className="label"
-                  label="Email"
-                  placeholder="joe123@abc.com"
-                  required={true}
-                  width={8}
-                />
-                <Form.Input
-                  className="label"
-                  label="Billing Address"
-                  placeholder="Billing Address"
-                  required={true}
-                  width={8}
-                />
-                <Form.Input
-                  className="label"
-                  label="CVV"
-                  placeholder="CVV"
-                  required={true}
-                  width={3}
-                />
-              </Form>
-              <Button type="submit">Proceed Payment</Button>
-            </Container>
-          </Grid.Column>
-        </Grid>
+            </Grid.Column>
+          </Grid>
+        </Segment>
       </div>
     );
   }
