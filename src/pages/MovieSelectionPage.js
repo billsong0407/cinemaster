2478150cfdccbd961ea2f6647aa373a8272f29e4
Marlon Liu <alienlql@hotmail.com>
2022-11-18 12:24:21 -0500

    add dropdown change

--- src/pages/MovieSelectionPage.js
+++ src/pages/MovieSelectionPage.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+
+import React, { Component } from 'react'
 import "../css/MovieSelection.css";
 import {
   Dropdown,
@@ -9,33 +10,67 @@ import {
   Segment,
 } from "semantic-ui-react";
 
+
 const movieOptions = [
-    {
-        key: '1',
-        text: 'Avengers',
-        value: 'Avengers',
-    },
-    {
-        key: '2',
-        text: 'Godzilla',
-        value: 'Godzilla',
-    },
-    {
-        key: '3',
-        text: 'pacific rim',
-        value: 'pacific rim',
-    },
+  {
+      key: '1',
+      text: 'Avengers',
+      value: 'Avengers',
+  },
+  {
+      key: '2',
+      text: 'Godzilla',
+      value: 'Godzilla',
+  },
+  {
+      key: '3',
+      text: 'pacific rim',
+      value: 'pacific rim',
+  },
 ]
+
+const movies= {
+     "Avengers" : {
+      title: "Avengers: Final Game",
+      description: 'Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe.',
+      category: 'Future, Superheros',
+      directors: 'Anthony Russo, Joe Russo',
+      cast: 'Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth, Scarlett Johansson...',
+      ratings: '12 +',
+      image: "/images/avengers.jpg"
+    }
+  }
 
 class MovieSelectionPage extends Component {
   constructor(props) {
     super(props);
+    this.default = "Avengers"
     this.state = {
-      cardNum: "",
-      cardHolder: "",
-      cvv: "",
-      billingAddress: "",
+      defaultMovie : this.default,
+      title: movies[this.default].title,
+      description: movies[this.default].description,
+      category: movies[this.default].category,
+      directors: movies[this.default].directors,
+      cast: movies[this.default].cast,
+      ratings: movies[this.default].ratings,
+      image: movies[this.default].image
     };
+  }
+
+  
+
+  movieOptions = (e, data) => {
+    const val = data.value
+    const movie = movies[val]
+    this.setState({
+      title: movie.title,
+      description: movie.description,
+      category: movie.category,
+      directors: movie.directors,
+      cast: movie.cast,
+      ratings: movie.ratings,
+      image: movie.image
+    });
   }
 
   render() {
@@ -51,7 +86,7 @@ class MovieSelectionPage extends Component {
                   padding: "5vh 20vh 5vh 20vh",
                 }}
               >
-                <h2> Avengers: Final Game </h2>
+                <h2> {this.state.title} </h2>
                 <div
                   style={{
                     display: "flex",
@@ -65,7 +100,7 @@ class MovieSelectionPage extends Component {
                         <Grid.Column>
                           <Container textAlign="left">
                             <h3>Category</h3>
-                            <p> Future, Superheros </p>
+                            <p> {this.state.category}</p>
                           </Container>
                         </Grid.Column>
                         </Grid.Row>
@@ -73,7 +108,7 @@ class MovieSelectionPage extends Component {
                         <Grid.Column>
                           <Container textAlign="left">
                             <h3>Description</h3>
-                            <p> Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe. </p>
+                            <p> {this.state.description} </p>
                           </Container>
                         </Grid.Column>
                         </Grid.Row>
@@ -81,7 +116,7 @@ class MovieSelectionPage extends Component {
                         <Grid.Column>
                           <Container textAlign="left">
                             <h4>Directors: </h4>
-                            <p> Anthony Russo, Joe Russo </p>
+                            <p> {this.state.directors} </p>
                           </Container>
                         </Grid.Column>
                       </Grid.Row>
@@ -89,15 +124,15 @@ class MovieSelectionPage extends Component {
                         <Grid.Column>
                           <Container textAlign="left">
                             <h4>Cast</h4>
-                            <p> Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth, Scarlett Johansson... </p>
+                            <p> {this.state.cast} </p>
                           </Container>
                         </Grid.Column>
                       </Grid.Row>
                       <Grid.Row>
                         <Grid.Column>
                           <Container textAlign="left">
-                            <h4>Suitable age</h4>
-                            <p> 12 and above </p>
+                            <h4>Rating</h4>
+                            <p> {this.state.ratings} </p>
                           </Container>
                         </Grid.Column>
                       </Grid.Row>
@@ -117,7 +152,7 @@ class MovieSelectionPage extends Component {
                 <Grid.Row>
                 <Container textAlign="center">
             
-                <Image src="/images/avengers.jpg" size="large" />
+                <Image src={this.state.image} size="large" />
                   
                 </Container>
                 </Grid.Row>
@@ -128,6 +163,7 @@ class MovieSelectionPage extends Component {
                         clearable
                         openOnFocus
                         selection
+                        defaultValue={this.state.defaultMovie}
                         options={movieOptions}
                         onChange={this.movieOptions}
                     />
